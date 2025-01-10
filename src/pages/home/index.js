73a00797c5d2552e6915913c676d42d2b5250887
stanislav <stanislav@getfocalpoint.com>
2025-01-11 00:21:39 +0200

    update resume

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,102 +1,109 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import screenSizes from "../../constants/screenSizes";
 import urls from "../../constants/urls";
-import { TitleAnim, Container, TagsTextDecorator, Button, Skills } from "../../components";
+import {
+  TitleAnim,
+  Container,
+  TagsTextDecorator,
+  Button,
+  Skills,
+} from "../../components";
 import useText from "../../hooks/useText.js";
-import { Helmet } from "react-helmet";
-import { useSelector } from "react-redux";
-
+import {Helmet} from "react-helmet";
+import {useSelector} from "react-redux";
 
 const ContainerStyled = styled(Container)`
-grid-template-columns: 1fr;
-`
+  grid-template-columns: 1fr;
+`;
 const TextWrapper = styled(TagsTextDecorator)`
-height: auto;
-`
+  height: auto;
+`;
 const TextContent = styled(TagsTextDecorator)`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;    
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 const Title = styled(TagsTextDecorator)`
-margin-bottom: 20px;
+  margin-bottom: 20px;
 
-h1{
+  h1 {
     margin-bottom: 0;
     color: var(--white);
-}
-`
+  }
+`;
 const ButtonContainer = styled(TagsTextDecorator)`
-display: flex;
-grid-column-gap: 20px;
-grid-row-gap: 20px;
-margin-top: 20px;
+  display: flex;
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+  margin-top: 20px;
 
-a{    
+  a {
     display: block;
-}
+  }
 
-@media screen and (max-width: ${screenSizes.MOBILE}){
+  @media screen and (max-width: ${screenSizes.MOBILE}) {
     flex-direction: column;
     max-width: 300px;
 
-    a{        
-        width: 100%;
+    a {
+      width: 100%;
 
-        button{
-            width: 100%;
-        }
+      button {
+        width: 100%;
+      }
     }
-}
+  }
 `;
 const ButtonViolet = styled(Button)`
-color: var(--violet);
+  color: var(--violet);
 
-&:hover{
+  &:hover {
     color: var(--white);
     border-color: var(--violet);
-}
+  }
 
-&:before{
+  &:before {
     background-color: var(--violet);
-}
+  }
 
-@media screen and (max-width: ${screenSizes.MOBILE}){
+  @media screen and (max-width: ${screenSizes.MOBILE}) {
     width: 100%;
-}
-`
+  }
+`;
 
+export default function HomePage() {
+  const lang = useSelector((state) => state.language.lang);
+  const text = useText();
 
-export default function HomePage(){
-    const lang = useSelector(state => state.language.lang);
-    const text = useText();
+  return (
+    <ContainerStyled>
+      <Helmet>
+        <title>Portfolio</title>
+      </Helmet>
 
-    return (
-        <ContainerStyled>
-            <Helmet>
-                <title>Portfolio</title>
-            </Helmet>
-
-            <TextWrapper tag={'html'}>
-                <TextContent tag={'body'}>
-                    <Title tag={'h1'}>
-                        <TitleAnim text={text('home.text.title.part1')} />
-                        <TitleAnim text={text('home.text.title.part2')} />
-                        <TitleAnim text={text('home.text.title.part3')} />
-                    </Title>
-                    <ButtonContainer>
-                        <a href={`/FrontendDeveloperKinshynStanislav${lang.toUpperCase()}.pdf`} download>
-                            <ButtonViolet>{text('home.text.button.resume')}</ButtonViolet>
-                        </a>
-                        <Link to={urls.contact}>
-                            <Button>{text('home.text.button.contact')}</Button>
-                        </Link>
-                    </ButtonContainer>
-                </TextContent>
-            </TextWrapper>
-            {/*<Skills />*/}
-        </ContainerStyled>
-    )
+      <TextWrapper tag={"html"}>
+        <TextContent tag={"body"}>
+          <Title tag={"h1"}>
+            <TitleAnim text={text("home.text.title.part1")} />
+            <TitleAnim text={text("home.text.title.part2")} />
+            <TitleAnim text={text("home.text.title.part3")} />
+          </Title>
+          <ButtonContainer>
+            <a
+              href={`/KinshynStanislavFrontendDeveloper${lang.toUpperCase()}.pdf`}
+              download
+            >
+              <ButtonViolet>{text("home.text.button.resume")}</ButtonViolet>
+            </a>
+            <Link to={urls.contact}>
+              <Button>{text("home.text.button.contact")}</Button>
+            </Link>
+          </ButtonContainer>
+        </TextContent>
+      </TextWrapper>
+      {/*<Skills />*/}
+    </ContainerStyled>
+  );
 }
